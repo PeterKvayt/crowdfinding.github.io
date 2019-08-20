95a462eebfa239d38dbc61f6f9c54a753e042756
PeterKvayt <peter.kvayt@gmail.com>
2019-08-20 23:38:16 +0300

    начал делать помощь и сделал rewardPage.html

--- js/rewardCard.js
+++ js/rewardCard.js
@@ -84,6 +84,50 @@ class RewardCard{
     );
   }
 
+  AddPayReward(selector){
+    $(selector).append(
+    '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">'+
+        '<div class="reward-card-box">'+
+          '<img class="reward-img" src="'+ this.Picture +'" alt="rewardImg"/>'+
+          '<div class="reward-card-body">'+
+            '<div class="reward-card-description-full">'+
+              '<div class="reward-title">'+ this.Name +'</div>'+
+              '<div class="description" style="display: none;">' + this.Description +'</div>' +
+              '<span class="link show-description-btn">Развернуть описание</span>'+
+            '</div>'+
+            '<div class="reward-card-pay-info">'+
+              '<div class="row">' +
+                '<div class="col">' +
+                  '<span class="card-down-status">Примерная дата доставки</span>' +
+                  '<span class="reward-card-description-full">' + this.DeliveryMonth + ' ' + this.DeliveryYear + '</span>' +
+                '</div>' +
+              '</div>' +
+              '<div class="row">' +
+                '<div class="col">' +
+                  '<span class="card-down-status">Способ получения</span>' +
+                  '<span class="reward-card-description-full">' + this.DeliveryType + '</span>' +
+                '</div>' +
+              '</div>' +
+              '<div class="row">' +
+                '<div class="col">' +
+                  '<div class="reward-cost">' + this.Price + ' BYN</div>' +
+                '</div>' +
+                '<div class="col">' +
+                  '<div class="rewards-left">' + this.Left + '</div>' +
+                '</div>' +
+              '</div>'+
+              '<div class="row">' +
+                '<div class="col">' +
+                  '<a class="link" href="#">Продолжить</a>' +
+                '</div>' +
+              '</div>'+
+            '</div>'+
+          '</div>'+
+        '</div>'+
+    '</div>'
+    )
+  }
+
   AddContolButtons(selector){
     $(selector).append(
       '<div class="row">'+
